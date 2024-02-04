a56e33951e6eb35354f3dfa7bc691b11aab64055
thehalfkoreanzombie <thehalfkoreanzombie@gmail.com>
2024-02-03 19:20:18 -0800

    holy smokes i figured it out. finished the dag

--- dsa-airflow/dags/party_dag.py
+++ dsa-airflow/dags/party_dag.py
@@ -5,39 +5,12 @@ from airflow import DAG
 from airflow.decorators import dag, task
 from airflow.sensors.filesystem import FileSensor
 from airflow.hooks.filesystem import FSHook
-import csv
+from statistics import mode
 
 #list of flavors people wanted at the party
 FLAVORS_CHOICES = ["lemon", "vanilla", "chocolate", "pistachio", "strawberry", "confetti", "caramel", "pumpkin", "rose"]
 
 VOTES_FILE_NAME = 'votes.csv'
-
-@task
-def read_and_check_votes():
-    """
-    read all of the votes from the CSV
-
-    This function uses an Airflow File(path) Connection called "data_fs" as the root folder
-    to look for the votes csv. Make sure this File(path) connection exists
-    """
-    # get the data_fs filesystem root path
-    data_fs = FSHook(fs_conn_id='data_fs')     # get airflow connection for data_fs
-    data_dir = data_fs.get_path()           # get its root path
-    print(f"data_fs root path: {data_dir}")
-
-    # create the full path to the airports file
-    file_path = os.path.join(data_dir, VOTES_FILE_NAME)
-    print(f"reading file: {file_path}")
-
-    # read csv
-    df = pd.read_csv(file_path, header=0)
-    all_votes = df['votes'].tolist()
-    print(all_votes)
-    valid_votes = []
-    for item in all_votes:
-        if item in FLAVORS_CHOICES:
-            valid_votes.append(item)
-    return valid_votes
 
 @dag(
     schedule_interval="@once",
@@ -45,14 +18,43 @@ def read_and_check_votes():
     catchup=False,
     default_view='graph',
     is_paused_upon_creation=True,
-    tags=['party_time'],
+    tags=['party_time']
 )
 
 def party_cake_choice():
-    """Example of using FileSensors and FileSystem Connections"""
+    
+    @task
+    def read_votes_with_return_value() -> str:
+        """
+        read all of the votes from the CSV
 
-    # define the file sensor...
-    # wait for the airports file in the "data_fs" filesystem connection
+        This function uses an Airflow File(path) Connection called "data_fs" as the root folder
+        to look for the votes csv. Make sure this File(path) connection exists
+        """
+        # get the data_fs filesystem root path
+        data_fs = FSHook(fs_conn_id='data_fs')     # get airflow connection for data_fs
+        data_dir = data_fs.get_path()           # get its root path
+        print(f"data_fs root path: {data_dir}")
+
+        # create the full path to the airports file
+        file_path = os.path.join(data_dir, VOTES_FILE_NAME)
+        print(f"reading file: {file_path}")
+
+        # read csv
+        df = pd.read_csv(file_path, header=0)
+        all_votes = df['votes'].tolist()
+        #print(all_votes)
+        valid_votes = []
+        for item in all_votes:
+            if item in FLAVORS_CHOICES:
+                valid_votes.append(item)
+        return valid_votes
+
+    @task
+    def most_voted_flavor(flavor_list: str):
+        flavor = mode(flavor_list)
+        print(f"The flavor of choice is {flavor}!!!")
+
     wait_for_file = FileSensor(
         task_id='wait_for_file',
         poke_interval=15,                   # check every 15 seconds
@@ -62,10 +64,12 @@ def party_cake_choice():
         fs_conn_id='data_fs',               # file system connection (root path)
     )
     
-    t1 = read_and_check_votes()
+    t1 = read_votes_with_return_value()
+
+    t2 = most_voted_flavor(t1)
     
     # orchestrate tasks
-    wait_for_file >> t1
+    wait_for_file >> t1 >> t2
 
-# create the dag
+    # create the dag
 dag = party_cake_choice()
